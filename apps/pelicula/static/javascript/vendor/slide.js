665288ce81fb7702f5d4e8563533a2d6c10547db
luixlacrux <luixlacrux18@gmail.com>
2015-12-16 01:47:33 -0430

    Mejorando SlideShow

--- apps/pelicula/static/javascript/vendor/slide.js
+++ apps/pelicula/static/javascript/vendor/slide.js
@@ -1,47 +1,23 @@
-var indicador = 0;
+$(function() {
+    var $contenedor =  $('.form_container');
+    var swift = true
 
-$(document).on('ready',function(){
-	$('.left').on('click', function(e){
-		e.preventDefault;
-		moveSlider('left');
-	});
-	$('.right').on('click', function(e){
-		e.preventDefault;
-		moveSlider('right');
-	});
-	//defineSizes();
+    $('.right').click(
+        function (e) {  
+            if (swift){
+                $contenedor.animate({scrollLeft: "800px"}, 2000);     
+            }
+            swift = false;
+      });
+
+    $('.left').click(
+        function (e) {
+          
+            if (swift == false){
+                $contenedor.animate({scrollLeft: "0px"}, 800);
+            }
+            swift = true;
+                
+      }
+    );
 });
-
-/*function defineSizes(){
-	$('.form_container .slide').each(function(i,el){
-		$(el).css({
-			'background-image': "url("+$(el).data("background")+")",
-			'height': (250 * 1)+'px',
-			'width': 123+'px'  
-		});
-	});
-}*/
-
-function moveSlider(direccion){
-	var limite = $('.form_container .slide').length;
-
-	if (direccion=='right'){
-		indicador++
-		if (indicador >= 5) {
-			indicador = 0
-		}
-	}else{	
-		indicador--
-		if (indicador < 0) {
-			indicador = limite -5; 
-		};
-	} 
-	/*indicador = (direccion == 'right') ? indicador + 1 : indicador - 1;
-	indicador = (indicador >= limite) ? 0 : indicador;
-	indicador = (indicador < 0) ? limite - 1 : indicador;
-*/
-	
-	$('.form_container .slideContainer').animate({
-		'margin-left': -(indicador * 520)+'px'
-	});
-}
